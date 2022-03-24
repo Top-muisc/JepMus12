6779ca1a2cd08f71bf3e02420b2ec5ef4a3554dd
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2022-03-24 13:23:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,8 @@ if os.path.exists(".env"):
 # الفارات
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
+OWNER_NAME = os.getenv("OWNER_NAME")
+CHANNEL = os.getenv("CHANNEL")
 SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", "$")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
